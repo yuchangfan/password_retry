41b0e1bdc759b24b1fa4a5178b2ca7173433f46b
Yuchang Fan <yuchangjimmyfan@gmail.com>
2021-08-08 00:44:54 +0800

    revised version 2.0

--- password_retry.py
+++ password_retry.py
@@ -1,19 +1,19 @@
 # 密碼重試程式
-# '最多輸入3次密碼'
-# 不對的話，印出'密碼錯誤！ 還有__次機會。'
-# 對的話，印出'登入成功！'
+# 最多輸入3次密碼
 
 password = 'a123456'
 remain = 3
-while reamain > 0:
+while remain > 0:
+	remain = remain - 1
 	enter = input('請輸入密碼： ')
 	if enter == password:
 		print('登入成功！')
 		break
 	else:
-		remain = remain - 1
-		print('密碼錯誤！還有',remain , '次機會')
-		if remain == 0:
+		print('密碼錯誤')
+		if remain > 0:
+			print('還有',remain , '次機會！')
+		else:
 			print('洗洗睡吧！')
 
 
